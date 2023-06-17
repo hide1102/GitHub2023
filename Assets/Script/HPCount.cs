cffa72c5e9e7d615dd311d2a8405a72a706ee425
hide1102 <136874206+hide1102@users.noreply.github.com>
2023-06-17 19:05:55 +0900

    NameFirst
    最初のbranchです

---
+++ Assets/Script/HPCount.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPCount : MonoBehaviour
+{
+    [SerializeField] private Text HPText;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
